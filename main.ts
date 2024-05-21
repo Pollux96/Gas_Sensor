591ba8df4dbfbdaaa24eef880d11162c2eca380e
Pollux96 <110056689+Pollux96@users.noreply.github.com>
2024-05-21 15:15:08 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ function Anzeige_Gas_Wert () {
     I2C_LCD1602.ShowString(convertToText(pins.analogReadPin(AnalogPin.P3)), 0, 1)
 }
 function Alarm (Aufruf: number) {
-    while (pins.analogReadPin(AnalogPin.P3) <= 600) {
+    if (Aufruf == 1) {
         I2C_LCD1602.ShowString("GAS Alarm:", 0, 0)
         Anzeige_Gas_Wert()
         music.ringTone(262)
@@ -15,5 +15,7 @@ I2C_LCD1602.LcdInit(39)
 basic.forever(function () {
     if (pins.analogReadPin(AnalogPin.P3) > 600) {
         Alarm(1)
+    } else if (pins.analogReadPin(AnalogPin.P3) <= 600) {
+        Alarm(0)
     }
 })
