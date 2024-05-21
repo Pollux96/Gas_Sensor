1766c56f5d7fade3d14ca94f4292d8fd5e1d4dd2
Pollux96 <110056689+Pollux96@users.noreply.github.com>
2024-05-21 15:00:55 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,21 @@
+function Anzeige_Gas_Wert () {
+    I2C_LCD1602.ShowString(convertToText(pins.analogReadPin(AnalogPin.P3)), 0, 1)
+}
+function Alarm (Aufruf: number) {
+    while (Aufruf == 1) {
+        I2C_LCD1602.ShowString("GAS Alarm:", 0, 0)
+        Anzeige_Gas_Wert()
+        music.ringTone(262)
+        basic.pause(100)
+        music.stopAllSounds()
+    }
+}
 basic.showIcon(IconNames.Heart)
+I2C_LCD1602.LcdInit(39)
 basic.forever(function () {
-    basic.showString("" + (pins.analogReadPin(AnalogPin.P3)))
+    if (pins.analogReadPin(AnalogPin.P3) > 600) {
+        Alarm(1)
+    } else if (pins.analogReadPin(AnalogPin.P3) <= 600) {
+        Alarm(0)
+    }
 })
